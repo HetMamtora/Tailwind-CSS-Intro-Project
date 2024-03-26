5fb96cd1fac0999e50b7bf8d32d4621f2663caa8
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-26 16:52:13 +0530

    updated

--- TailwindCSS-Landing-Page/src/App.jsx
+++ TailwindCSS-Landing-Page/src/App.jsx
@@ -1,9 +1,16 @@
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Body from "./Components/Body"
+
 function App() {
 
   return (
     <>
-      <div>
-        Hello World
+      <div className="bg-amber-100 min-h-screen px-6 py-3 flex flex-col gap-16">
+        <Header />
+        <Body />
+        <Footer />
+        
       </div>
     </>
   )
